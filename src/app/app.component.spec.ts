61dd09ab7fe92272f5dcb62a9d51fc2f9bb42068
Alex Sfamurri <alex.sfamurri.504@gmail.com>
2018-10-01 12:21:48 -0500

    s some saves after the merge

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -13,26 +13,15 @@ describe('AppComponent', () => {
     const app = fixture.debugElement.componentInstance;
     expect(app).toBeTruthy();
   }));
-<<<<<<< HEAD
-  it(`should have as title 'HomeFit'`, async(() => {
-    const fixture = TestBed.createComponent(AppComponent);
-    const app = fixture.debugElement.componentInstance;
-    expect(app.title).toEqual('HomeFit');
-=======
   it(`should have as title 'Take a Lap'`, async(() => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.debugElement.componentInstance;
     expect(app.title).toEqual('Take a Lap');
->>>>>>> d46a17f3a9c3e97f14ac34a1e58e31989c41fd4c
   }));
   it('should render title in a h1 tag', async(() => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.debugElement.nativeElement;
-<<<<<<< HEAD
-    expect(compiled.querySelector('h1').textContent).toContain('Welcome to HomeFit!');
-=======
     expect(compiled.querySelector('h1').textContent).toContain('Terrible, Take a Lap!');
->>>>>>> d46a17f3a9c3e97f14ac34a1e58e31989c41fd4c
   }));
 });
