b09f2b1a1699a321add2f73c4117ded1c95d98aa
Joshua Strzeszkowski <joshstrzeszkowski@gmail.com>
2018-10-01 15:28:46 -0500

    working on complete redirection between pages with temporary buttons

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,11 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
+  constructor(private router: Router) { };
+
+  onSubmit() {
+    this.router.navigate(['/home'])
+    console.log('clicked')
+  }
   title = 'HomeFit';
 }
