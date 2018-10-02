7639483327c0177bb362a0716320ba897e00493f
Alex Sfamurri <alex.sfamurri.504@gmail.com>
2018-10-02 13:57:06 -0500

    noticed that somone made another nother server folder so i moved the database folder out of the backend folder and made modifications to the indexjs in the backend folder. will probably use this one as a test platform to avoid collisions until necessary

--- back-end/server/index.js
+++ back-end/server/index.js
@@ -1,6 +1,6 @@
 const express = require('express')
 const bodyParser = require('body-parser');
-const db = require('../database/dbHelpers');
+const db = require('../../database/dbHelpers');
 const app = express()
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({
