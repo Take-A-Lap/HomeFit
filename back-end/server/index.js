d9b5a269b5751b206009663980aad5aa2d0b56b0
Roger Journee <rogerjournee@gmail.com>
2018-10-02 14:18:59 -0500

    Added static files to server

--- back-end/server/index.js
+++ back-end/server/index.js
@@ -1,12 +1,15 @@
 const express = require('express')
 const bodyParser = require('body-parser');
 const app = express();
+
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({
   extended: true
 }));
-const port = 3000
+
+const port = 3000;
+app.use(express.static('dist/HomeFit'));
 
 app.get('/', (req, res) => res.send('Hello World!'));
 
-app.listen(port, () => console.log(`Example app listening on port ${port}!`))
+app.listen(port, () => console.log(`Example app listening on port ${port}!`));
