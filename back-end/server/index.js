7015a4a695654f4e45e6c7dd9d0e1c8440a95e7b
Alex Sfamurri <alex.sfamurri.504@gmail.com>
2018-10-02 09:59:17 -0500

    moved the server files to the proper server folder

--- back-end/server/index.js
+++ back-end/server/index.js
@@ -1,5 +1,6 @@
 const express = require('express')
 const bodyParser = require('body-parser');
+const db = require('../database/dbHelpers');
 const app = express()
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({
@@ -8,5 +9,14 @@ app.use(bodyParser.urlencoded({
 const port = 3000
 
 app.get('/', (req, res) => res.send('Hello World!'))
+
+// TODO: create routes that can be used if for nothing else testing
+
+app.get('/test', (req, res) => {
+  db.getUserById(1)
+  .then(({ userArr }) => {
+    send(userArr);
+  })
+});
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
