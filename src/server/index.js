7cc7aa2cb0ea7ffb9f8a03607e71d52a827bb966
Cornelius <brcornelius@gmail.com>
2018-10-01 19:44:29 -0500

    included body parser

--- src/server/index.js
+++ src/server/index.js
@@ -1,5 +1,10 @@
 const express = require('express')
+const bodyParser = require('body-parser');
 const app = express()
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({
+  extended: true
+}));
 const port = 3000
 
 app.get('/', (req, res) => res.send('Hello World!'))
