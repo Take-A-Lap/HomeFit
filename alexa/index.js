fcde1de1c4dca823daf8985f4928f4e3e77fc453
Alex Sfamurri <alex.sfamurri.504@gmail.com>
2018-10-03 15:06:23 -0500

    added three other handlers for amazon intents that cancel or end the skill end the session and handle errors

--- alexa/index.js
+++ alexa/index.js
@@ -42,10 +42,54 @@ const ReadWorkoutStatusHandler = {
   }
 };
 
+const CancelAndStopIntentHandler = {
+  canHandle(handlerInput) {
+    return handlerInput.requestEnvelope.request.type === 'IntentRequest'
+      && (handlerInput.requestEnvelope.request.intent.name === 'AMAZON.CancelIntent'
+        || handlerInput.requestEnvelope.request.intent.name === 'AMAZON.StopIntent');
+  },
+  handle(handlerInput) {
+    const speechText = 'Goodbye!';
+
+    return handlerInput.responseBuilder
+      .speak(speechText)
+      .withSimpleCard('Hello World', speechText)
+      .getResponse();
+  }
+};
+
+const SessionEndedRequestHandler = {
+  canHandle(handlerInput) {
+    return handlerInput.requestEnvelope.request.type === 'SessionEndedRequest';
+  },
+  handle(handlerInput) {
+    //any cleanup logic goes here
+    return handlerInput.responseBuilder.getResponse();
+  }
+};
+
+const ErrorHandler = {
+  canHandle() {
+    return true;
+  },
+  handle(handlerInput, error) {
+    console.log(`Error handled: ${error.message}`);
+
+    return handlerInput.responseBuilder
+      .speak('Sorry, I can\'t understand the command. Please say again.')
+      .reprompt('Sorry, I can\'t understand the command. Please say again.')
+      .getResponse();
+  },
+};
+
 exports.handler = Alexa.SkillBuilders.custom()
   .addRequestHandlers(
     //handlers go here
     LaunchRequestHandler,
     StartWorkoutHandler,
-    RecomendRecipeHandler
+    RecommendRecipeHandler,
+    ReadWorkoutStatusHandler,
+    CancelAndStopIntentHandler,
+    SessionEndedRequestHandler,
+    ErrorHandler
   ).create();
