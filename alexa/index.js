4c1e0700466cc3c9991621544462f2ba5a4c40ee
Alex Sfamurri <alex.sfamurri.504@gmail.com>
2018-10-03 15:00:53 -0500

    added the readworkoutstatushandler hoping this will all be able to work

--- alexa/index.js
+++ alexa/index.js
@@ -24,7 +24,7 @@ const StartWorkoutHandler = {
   }
 };
 
-const RecomendRecipeHandler = {
+const RecommendRecipeHandler = {
   canHandle(handlerInput) {
 
   },
@@ -33,6 +33,14 @@ const RecomendRecipeHandler = {
   }
 };
 
+const ReadWorkoutStatusHandler = {
+  canHandle(handlerInput) {
+
+  },
+  handle(handlerInput) {
+
+  }
+};
 
 exports.handler = Alexa.SkillBuilders.custom()
   .addRequestHandlers(
