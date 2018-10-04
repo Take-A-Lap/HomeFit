34936db15296b9699935d40744a658dea93ec4ca
Cornelius <brcornelius@gmail.com>
2018-10-04 13:51:53 -0500

    normal

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,14 +1,41 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
+import { HttpClientModule } from '@angular/common/http';
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
 
+@Injectable()
+export class ConfigService {
+  constructor(private http: HttpClient) { }
+}
+@NgModule({
+  imports: [
+    BrowserModule,
+    // import HttpClientModule after BrowserModule.
+    HttpClientModule,
+  ],
+  declarations: [
+    // AppComponent,
+  ],
+  // bootstrap: [AppComponent]
+})
+export class AppModule { }
 @Component({
   selector: 'app-home-component',
   templateUrl: './home.component.html',
   styles: []
 })
 export class HomeComponent implements OnInit {
-
+  onClick() {
+    console.log('hello');
+    // return this.http.get('/signupWO', { responseType: 'array' })
+    
+    // );
+  }
   ngOnInit() {
   }
 
+  
 }
