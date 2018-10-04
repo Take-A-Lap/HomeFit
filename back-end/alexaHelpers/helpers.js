f43782449e977699e58f71fc58022bfddd0b5526
Alex Sfamurri <alex.sfamurri.504@gmail.com>
2018-10-04 08:33:30 -0500

    updated the build response functions signatures

--- back-end/alexaHelpers/helpers.js
+++ back-end/alexaHelpers/helpers.js
@@ -1,8 +1,8 @@
-const buildResponse = () => {
+const buildResponse = (speechText, shouldSessionEnd, cardText) => {
   // TODO: create a way to build the response in json
 };
 
-const buildResponseWithPrompt = () => {
+const buildResponseWithPrompt = (speechText, shouldSessionEnd, cardText, reprompt) => {
   // TODO: create a way to build the response with a prompt in json
 };
 
@@ -10,7 +10,7 @@ module.exports = {
   invocationIntent: () => {
     const greetingSpeech = 'Welcome to Alexa Fitness Trainer. When you are ready to begin your workout just let me know, or if you rather check out what I recommend you eat for the day you can ask me that as well. So what would you like to do?'
     // TODO: build this out to handle more
-    return buildResponseWithPrompt();
+    return buildResponseWithPrompt(greetingSpeech, false, 'Welcome');
   }
 
 };
