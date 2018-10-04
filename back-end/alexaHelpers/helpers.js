091a52a46397a67b7f43f0d3cf37800c2a93f962
Alex Sfamurri <alex.sfamurri.504@gmail.com>
2018-10-04 08:18:23 -0500

    realized it would probably be better if the functions that are exported are tailered to the intent more and the response builders just live in the helper.js folder

--- back-end/alexaHelpers/helpers.js
+++ back-end/alexaHelpers/helpers.js
@@ -1,5 +1,12 @@
+const buildResponse = () => {
+  // TODO: create a way to build the response in json
+};
+
+const buildResponseWithPrompt = () => {
+  // TODO: create a way to build the response with a prompt in json
+};
+
 module.exports = {
-  buildResponse: () => {},
-  buildResponseWithPrompt: () => {},
   
+
 };
