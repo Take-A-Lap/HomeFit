c53a716d223a6e29a81950dd3f6ca372a76c9106
Joshua Strzeszkowski <joshstrzeszkowski@gmail.com>
2018-10-05 02:50:44 -0500

    recording settings information

--- src/app/settings-personal-info/settings-personal-info.component.ts
+++ src/app/settings-personal-info/settings-personal-info.component.ts
@@ -10,6 +10,10 @@ export class SettingsPersonalInfoComponent implements OnInit {
   age = '';
   height = '';
   weight = '';
+  goals = '';
+  push_ups = '';
+  squats = '';
+  miles = '';
 
   updateAge(e) {
     this.age = e.target.value;
@@ -25,7 +29,35 @@ export class SettingsPersonalInfoComponent implements OnInit {
     this.weight = e.target.value;
     console.log(this.weight)
   }
+  
+  updateGoals(e) {
+    this.updateGoals = e.target.value;
+    console.log(e.target.value);
+  }
 
+  updatePushUps(e) {
+    this.push_ups = e.target.value;
+  }
+
+  updateSquats(e) {
+    this.squats = e.target.value;
+  }
+
+  updateMiles(e) {
+    this.miles = e.target.value;
+  }
+
+  params = {
+    age: this.age,
+    height: this.height,
+    weight: this.weight,
+    goals: this.goals,
+    push_ups: this.push_ups,
+    squats: this.squats,
+    miles: this.miles
+  }
+
+  
   constructor() { }
 
   ngOnInit() {
