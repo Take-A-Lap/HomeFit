aea3b0cc323bb99dd4dc279ccdead7df563d8291
Joshua Strzeszkowski <joshstrzeszkowski@gmail.com>
2018-10-05 08:36:48 -0500

    strength data rendering

---
+++ src/app/mock-strength.ts
@@ -0,0 +1,68 @@
+import { Strength } from './strength';
+
+export const STRENGTH: Strength[] = [
+  {
+    id: 44,
+    name: "Tricep Pushup",
+    rep_time: 1200,
+    youtube_link: "https://www.youtube.com/embed/L99AyyEDBYA",
+    id_muscle_group: 5,
+    difficulty: 2
+  },
+  {
+    id: 44,
+    name: "Tricep Pushup",
+    rep_time: 1200,
+    youtube_link: "https://www.youtube.com/embed/L99AyyEDBYA",
+    id_muscle_group: 5,
+    difficulty: 2
+  },
+  {
+    id: 22,
+    name: "LegRaises",
+    rep_time: 1400,
+    youtube_link: "https://www.youtube.com/embed/Wp4BlxcFTkE",
+    id_muscle_group: 2,
+    difficulty: 2
+  },
+  {
+    id: 22,
+    name: "LegRaises",
+    rep_time: 1400,
+    youtube_link: "https://www.youtube.com/embed/Wp4BlxcFTkE",
+    id_muscle_group: 2,
+    difficulty: 2
+  },
+  {
+    id: 52,
+    name: "Pushups",
+    rep_time: 1900,
+    youtube_link: "https://www.youtube.com/embed/ZWdBqFLNljc",
+    id_muscle_group: 6,
+    difficulty: 1
+  },
+  {
+    id: 54,
+    name: "Decline Pushups",
+    rep_time: 1700,
+    youtube_link: "https://www.youtube.com/embed/WBmXffkfcXQ",
+    id_muscle_group: 6,
+    difficulty: 2
+  },
+  {
+    id: 47,
+    name: "Flies",
+    rep_time: 1800,
+    youtube_link: "https://www.youtube.com/embed/tJ2kaO29NDM",
+    id_muscle_group: 6,
+    difficulty: 1
+  },
+  {
+    id: 47,
+    name: "Flies",
+    rep_time: 1800,
+    youtube_link: "https://www.youtube.com/embed/tJ2kaO29NDM",
+    id_muscle_group: 6,
+    difficulty: 1
+  }
+]
--- src/app/strength/strength.component.ts
+++ src/app/strength/strength.component.ts
@@ -1,12 +1,23 @@
 import { Component, OnInit } from '@angular/core';
+import { Strength } from '../strength';
+import { STRENGTH } from '../mock-strength';
 
 @Component({
   selector: 'app-strength',
   templateUrl: './strength.component.html',
   styles: []
 })
+
+// const exercises = [
+//   {},
+// ];
 export class StrengthComponent implements OnInit {
 
+  exercises = STRENGTH;
+
+  switchExercise() {
+
+  }
   constructor() { }
 
   ngOnInit() {
