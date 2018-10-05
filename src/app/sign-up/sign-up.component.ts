8f46d7085357b12b8137286e7e9b3dfc3d47bcdd
Joshua Strzeszkowski <joshstrzeszkowski@gmail.com>
2018-10-05 14:24:51 -0500

    created redirect function for testing Alexa

--- src/app/sign-up/sign-up.component.ts
+++ src/app/sign-up/sign-up.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-sign-up',
@@ -7,8 +8,11 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SignUpComponent implements OnInit {
 
-  constructor() { }
+  constructor(private router: Router) { }
 
+  changePage() {
+    this.router.navigate(['/home']);
+  }
   ngOnInit() {
   }
 
