164f68f1ff9f8898d03d0eefd7b9698bdb41601e
Cornelius <brcornelius@gmail.com>
2018-10-05 01:00:47 -0500

    merged

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,12 +1,11 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
-
+import { FormsModule } from '@angular/forms';
 import { AppComponent } from './app.component';
 import { AppRoutingModule, routingComponents } from './app-routing.module';
 import { ErrorComponent } from './error/error.component';
 import { SettingsComponent } from './settings/settings.component';
 import { SignUpComponent } from './sign-up/sign-up.component';
-import { SettingsPersonalInfoComponent } from './settings-personal-info/settings-personal-info.component';
 import { DietaryRestrictionsComponent } from './dietary-restrictions/dietary-restrictions.component';
 import { StrengthComponent } from './strength/strength.component';
 import { CardioComponent } from './cardio/cardio.component';
@@ -19,7 +18,6 @@ import { BreakfastComponent } from './breakfast/breakfast.component';
     AppComponent,
     routingComponents,
     SignUpComponent,
-    SettingsPersonalInfoComponent,
     DietaryRestrictionsComponent,
     StrengthComponent,
     CardioComponent,
