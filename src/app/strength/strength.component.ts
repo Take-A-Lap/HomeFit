df0212ba70f3ee7134da4a32fc290431e13a7e89
Joshua Strzeszkowski <joshstrzeszkowski@gmail.com>
2018-10-05 10:49:20 -0500

    youtube embed working

--- src/app/strength/strength.component.ts
+++ src/app/strength/strength.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Strength } from '../strength';
 import { STRENGTH } from '../mock-strength';
+import { DomSanitizer, SafeResourceUrl, SafeUrl } from '@angular/platform-browser';
 
 @Component({
   selector: 'app-strength',
@@ -13,52 +14,61 @@ import { STRENGTH } from '../mock-strength';
 // ];
 export class StrengthComponent implements OnInit {
 
+  
   exercises = STRENGTH;
   index = 0;
   exercise = STRENGTH[this.index];
   completed = '';
   rep = 0;
   set = 1;
-
-  plus(){
-    let repIncrement = setInterval(() => {
-    if (this.rep < 10) {
-      this.rep++;
-    } else {
-        clearInterval(repIncrement);
-        this.rep = 0;
-      }
-    }, this.exercise.rep_time)
-  }
-
-  switchRep() {
-    this.plus();
-  }
-
-  workinDatBody(){
-    while(this.set < 4){
-
-    }
-  }
   
-  switchExercise() {
-    console.log(STRENGTH.length)
-    console.log(this.index);
-      this.exercise = STRENGTH[this.index];
+  youtube = this.exercise.youtube_link
+  trustedUrl: SafeUrl;
+  
+  constructor(private sanitizer: DomSanitizer) {
   }
-
-  increment() {
-    this.index++;
-    if (this.index < 5) {
-      this.switchExercise();
-    } else {
-      this.completed = 'Workout Complete';
+  // sanitizeAndEmbedURL(link){
+    //   this.sanitizer.bypassSecurityTrustUrl(link);
+    // }
+    
+    plus(){
+      let repIncrement = setInterval(() => {
+        if (this.rep < 10) {
+          this.rep++;
+        } else {
+          clearInterval(repIncrement);
+          this.rep = 0;
+        }
+      }, this.exercise.rep_time)
     }
-  }
-
-  constructor() { }
-
-  ngOnInit() {
-  }
+    
+    switchRep() {
+      this.plus();
+    }
+    
+    workinDatBody(){
+      while(this.set < 4){
+        
+      }
+    }
+    
+    switchExercise() {
+      console.log(STRENGTH.length)
+      console.log(this.index);
+      this.exercise = STRENGTH[this.index];
+    }
+    
+    increment() {
+      this.index++;
+      if (this.index < 5) {
+        this.switchExercise();
+      } else {
+        this.completed = 'Workout Complete';
+      }
+    }
+    
+    ngOnInit() {
+      this.trustedUrl = this.sanitizer.bypassSecurityTrustResourceUrl(this.youtube);
+}
 
 }
