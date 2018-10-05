5dd6bd225bbf7d65f7cadb9d249f05b01b1e1f3c
Roger Journee <rogerjournee@gmail.com>
2018-10-05 00:26:18 -0500

    Added sse.js file

---
+++ sse.js
@@ -0,0 +1,18 @@
+// function to call to send data to the front end
+// once the front end recieves 
+module.exports.workOutEvent = function (req, res, next) {
+    res.sseSetup = function() {
+        res.writeHead(200, {
+            'Content-Type': 'text/event-stream',
+            'Cache-Control': 'no-cache',
+            'Connection': 'keep-alive'
+        })
+    }
+
+    res.sseSend = function(data) {
+        res.write("event: workOutEvent\n")
+        res.write("data: " + JSON.stringify(data) + "\n\n");
+    }
+
+    next()
+}
