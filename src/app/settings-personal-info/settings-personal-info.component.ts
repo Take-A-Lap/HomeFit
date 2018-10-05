958508f12899775474ae2f54cf4ccf8f79a67532
Joshua Strzeszkowski <joshstrzeszkowski@gmail.com>
2018-10-05 01:45:24 -0500

    ngModel capturing input data in settings

--- src/app/settings-personal-info/settings-personal-info.component.ts
+++ src/app/settings-personal-info/settings-personal-info.component.ts
@@ -7,6 +7,25 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SettingsPersonalInfoComponent implements OnInit {
 
+  age = '';
+  height = '';
+  weight = '';
+
+  updateAge(e) {
+    this.age = e.target.value;
+    console.log(this.age)
+  }
+
+  updateHeight(e) {
+    this.height = e.target.value;
+    console.log(this.height)
+  }
+
+  updateWeight(e) {
+    this.weight = e.target.value;
+    console.log(this.weight)
+  }
+
   constructor() { }
 
   ngOnInit() {
