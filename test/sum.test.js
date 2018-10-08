0aed484a84a8e5213e868b19e6fb79ca77a57c80
Alex Sfamurri <alex.sfamurri.504@gmail.com>
2018-10-08 16:23:33 -0500

    added a todo comment to the sum.test.js

--- test/sum.test.js
+++ test/sum.test.js
@@ -3,3 +3,7 @@ const sum = require('../sum');
 test('adds 1 + 2 to equal 3', () => {
   expect(sum(1, 2)).toBe(3);
 });
+
+/**
+ * TODO: figure out how to best make tests for our site while maintaining our keys and passwords security
+ */
