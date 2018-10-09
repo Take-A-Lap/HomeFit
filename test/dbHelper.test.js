bd150a793e4db96a63d26ef6489ff61043b05829
Alex Sfamurri <alex.sfamurri.504@gmail.com>
2018-10-08 21:14:33 -0500

    realized i was using the done() incorrectly

--- test/dbHelper.test.js
+++ test/dbHelper.test.js
@@ -1,14 +1,10 @@
 const db = require('../back-end/database/dbHelpers');
 
-
-
 test('testing our test', ()=>{
   expect.assertions(1);
   return db.getUserById(1).then(userArr => {
     expect(userArr[0].id).toBe(1);
-    done();
   }).catch(err=>{
     console.error(err);
-    done();
   })
 })
