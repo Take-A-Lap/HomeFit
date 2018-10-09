084c5a9ff6774cf53fbd26a82ed189132e8cb653
joshua <joshstrzeszkowski@gmail.com>
2018-10-09 19:14:27 +0000

    before pull 2:14, 10/8

--- back-end/server/index.js
+++ back-end/server/index.js
@@ -26,16 +26,10 @@ app.use(bodyParser.urlencoded({
 
 //   console.log('I have a connection');
 
-<<<<<<< HEAD
-//   sseRes.sseSetup();
-
-  // sseRes.sseSend("Hey Again, I can connect more than once");
-=======
-  sseRes.sseSetup();
+ // sseRes.sseSetup();
   // fire off events
-  sseRes.sseSend("Hey Again, I can connect more than once");
+ // sseRes.sseSend("Hey Again, I can connect more than once");
   // sseRes.newEvent("We Got More Data");
->>>>>>> 35bcfcca14ffd606cf8dba68f6a754625acecfdd
 
   // attach the verifier middleware first because it needs the entire
   // request body, and express doesn't expose this on the request object
