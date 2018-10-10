9db8e7878c50cf3adee5121190352764fbb5d100
Alex Sfamurri <alex.sfamurri.504@gmail.com>
2018-10-10 16:50:36 -0500

    added some logs to the amazon reqquired intents to make sure which are being called and what is in them

--- back-end/server/index.js
+++ back-end/server/index.js
@@ -33,7 +33,7 @@ app.get('/.well-known/pki-validation/7BACD9E3D66343D40FE18A33C2899CB3.txt', (req
   let sets = 0;
   let current;
 alexaRouter.post('/fitnessTrainer', (req, res) => {
-  console.log(req.body.request.type, " this si the type of the request body")
+  console.log(req.body.request.type, " this us the type of request")
   if (req.body.request.type === 'LaunchRequest') {
     // console.log(req.body, ' line 16 server index');
     db.getUserInfoByAlexUserId(req.body.session.user.userId)
@@ -158,16 +158,21 @@ alexaRouter.post('/fitnessTrainer', (req, res) => {
           res.json(alexaHelp.changeView(view));
           break;
         case 'skipExercise':
-          console.log(worouts, " this should hold the list of workouts that are left incase we wish to skip to the next workout")
+          console.log(workouts, " this should hold the list of workouts that are left incase we wish to skip to the next workout")
+        console.log(req.body.request.intent, " ||||-----|||| this is skip exercise");
           res.json(alexaHelp.PLACEHOLDER());
           break;
         case 'AMAZON.FallbackIntent':
+        console.log(req.body.request.intent, " ||||-----|||| this is the amazon fallback intent");
+          
           res.json(alexaHelp.PLACEHOLDER());
           break;
         case 'AMAZON.HelpIntent':
+        console.log(req.body.request.intent, "||||-----|||| this is the amazon help intent");
           res.json(alexaHelp.PLACEHOLDER());
           break;
         case 'AMAZON.NavigateHomeIntent':
+        console.log(req.body.request.intent, "||||-----|||| this is the amazon navigate home intent");
           res.json(alexaHelp.PLACEHOLDER());
           break;
       default:
