2c875b42979d437938744b3d0fc6a9b1bc67dfc4
Alex Sfamurri <alex.sfamurri.504@gmail.com>
2018-10-13 15:41:42 -0500

    testing out google ass to get a feel for how to interact with her

--- back-end/googleAssHelpers/helpers.js
+++ back-end/googleAssHelpers/helpers.js
@@ -3,6 +3,8 @@ const {
   Image,
 } = require('actions-on-google')
 const db = require('../database/dbHelpers');
+const workout = require('../Algorithms/workout.js');
+
 
 const app = dialogflow();
 
@@ -10,6 +12,12 @@ const app = dialogflow();
 app.intent('start workout', conv => {
   console.log('please tell me this is at least called');
   conv.ask('This is a test to see that you are connected to the server');
+  conv.ask('Hi, how is it going?')
+  conv.ask(`Here's a picture of a cat`)
+  conv.ask(new Image({
+    url: 'https://developers.google.com/web/fundamentals/accessibility/semantics-builtin/imgs/160204193356-01-cat-500.jpg',
+    alt: 'A cat',
+  }))
 });
 
 app.intent('Default Fallback Intent', conv => {
