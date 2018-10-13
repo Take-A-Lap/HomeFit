94a670e8e0d6e572edf6720fa3f397d161e7aeb2
Alex Sfamurri <alex.sfamurri.504@gmail.com>
2018-10-13 18:30:02 -0500

    added the next exercise intent and commiting before the game

--- back-end/googleAssHelpers/helpers.js
+++ back-end/googleAssHelpers/helpers.js
@@ -13,20 +13,15 @@ console.log(app, ' let\'s see whats inside of this on line 11 in google helpers'
 
 app.intent('start workout', conv => {
   console.log(conv);
-  
-  // console.log('please tell me this is at least called');
-  // conv.ask('This is a test to see that you are connected to the server');
-  // conv.ask('Hi, how is it going?');
-  // conv.ask(`Here's a picture of a cat`);
-  // conv.ask(new Image({
-  //   url: 'https://developers.google.com/web/fundamentals/accessibility/semantics-builtin/imgs/160204193356-01-cat-500.jpg',
-  //   alt: 'A cat',
-  // }));
-  console.log('what happened after image was called?')
+  // need to remember to grab the conversation id
   conv.ask(new SimpleResponse({
     text: 'Let me know when you are ready to begin.',
     speech: '<speak> <s> Let me know when you are ready to begin. </s> </speak>'
   }));
+});
+
+app.intent('next exercise', conv => {
+
 });
 
 app.intent('Default Fallback Intent', conv => {
