cb3732c5d7754eed6ad041f5b016d5cd33e6c9cf
Alex Sfamurri <alex.sfamurri.504@gmail.com>
2018-10-13 16:46:57 -0500

    not sure exactly what google is doing

--- back-end/googleAssHelpers/helpers.js
+++ back-end/googleAssHelpers/helpers.js
@@ -14,16 +14,16 @@ console.log(app, ' let\'s see whats inside of this on line 11 in google helpers'
 app.intent('start workout', conv => {
   console.log('please tell me this is at least called');
   conv.ask('This is a test to see that you are connected to the server');
-  conv.ask('Hi, how is it going?')
-  conv.ask(`Here's a picture of a cat`)
+  conv.ask('Hi, how is it going?');
+  conv.ask(`Here's a picture of a cat`);
   conv.ask(new Image({
     url: 'https://developers.google.com/web/fundamentals/accessibility/semantics-builtin/imgs/160204193356-01-cat-500.jpg',
     alt: 'A cat',
-  }))
+  }));
   conv.ask(new SimpleResponse({
     text: '<s> Let me know when you are ready to begin.</s>',
     speech: '<s> Let me know when you are ready to begin.</s>'
-  }))
+  }));
 });
 
 app.intent('Default Fallback Intent', conv => {
