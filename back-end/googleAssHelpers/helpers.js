727aa50cfe04d357b4aea9edcd0c8da4c5970fcc
Alex Sfamurri <alex.sfamurri.504@gmail.com>
2018-10-14 21:43:25 -0500

    made some adjusmments to the script

--- back-end/googleAssHelpers/helpers.js
+++ back-end/googleAssHelpers/helpers.js
@@ -81,7 +81,7 @@ app.intent('next exercise', conv => {
   .then(user => {
     if (user !== undefined) {
       if (current !== undefined){
-        let cadence = `<speak> <s> The recommended pace for ${current.name} is ${current.rep_time * 1000} seconds. </s> <s> Let's begin on the count of 3. </s> 1 <break time="1s"/> 2 <break time="1s"/> <s> 3 </s>`;
+        let cadence = `<speak> <s> The recommended pace for ${current.name} is ${current.rep_time / 1000} seconds. </s> <s> Let's begin on the count of 3. </s> 1 <break time="500ms"/> 2 <break time="500ms"/> <s> 3 <break time="500ms"/> Let's begin </s>`;
         for (let i = 1; i < 11; i++) {
           cadence += ` give me a ${i} <break time="${current.rep_time}ms"/>`;
         }
