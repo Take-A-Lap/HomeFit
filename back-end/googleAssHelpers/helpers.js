3ac8668d22932905a2e768ff046590aa6795d3c6
Cornelius <brcornelius@gmail.com>
2018-10-17 15:44:32 -0500

    recover workout functional

--- back-end/googleAssHelpers/helpers.js
+++ back-end/googleAssHelpers/helpers.js
@@ -17,13 +17,9 @@ const errorResponses = [`<speak> <p> <s> I'm sorry, I may have miss heard you. <
   `<speak> <p> I am terribly sorry </p> <p> I am having trouble understanding you </p> <p> <prosody pitch="+5%"> If it isn't </prosody><prosody pitch="+15%"> too much to ask </prosody> <break time="5ms" /> <prosody  pitch="+30%">could you try again? </prosody> </p> </speak>`,
   `<speak> <s> This is embarrassing for me </s> <p> I sometimes have trouble with my hearing </p> <p> even at such a young age </p> <s> Would you kindly try the command again? </s> </speak>`
 ];
-<<<<<<< HEAD
-const spanishErrorResponse = [];
-=======
 
 const spanishErrorResponse = [`<speak> <p> <s> Lo siento mucho, no te puedo entender. </s> <s> <prosody pitch="+10%> ¿Podrías preguntarme otra vez? </prosody> </s> </p> </speak>`];
 
->>>>>>> 0b13b79541a06190f6c980c923efa1e5faf6e6e0
 const linkAccountObjResponses = [
   {
     before: '<speak> <s> Thank you </s> <s> ',
