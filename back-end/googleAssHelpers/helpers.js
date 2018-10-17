f57057605c44575f39059d4f647cbd3f9c16310a
Alex Sfamurri <alex.sfamurri.504@gmail.com>
2018-10-17 15:17:35 -0500

    added some styling to some more of the scripts

--- back-end/googleAssHelpers/helpers.js
+++ back-end/googleAssHelpers/helpers.js
@@ -13,8 +13,8 @@ const randomNumGen = (numOptions) => {
 };
 const app = dialogflow();
 
-const errorResponses = [`<speak> <p> <s> I'm sorry, I may have miss heard you. </s> <s> <prosody pitch="+10%" >Could you </prosody> <prosody pitch="+20%" > try again? </prosody></s> </p> </speak>`,
-  `<speak> <p> I am terribly sorry </p> <p> I am having trouble understanding you </p> <p> If it isn't too much to ask could you try again? </p> </speak>`,
+const errorResponses = [`<speak> <p> <s> I'm sorry, I may have miss heard you. </s> <s> <prosody pitch="+5%" >Could you </prosody> <prosody pitch="+25%" > try again? </prosody></s> </p> </speak>`,
+  `<speak> <p> I am terribly sorry </p> <p> I am having trouble understanding you </p> <p> <prosody pitch="+5%"> If it isn't </prosody><prosody pitch="+15%"> too much to ask </prosody> <break time="5ms" /> <prosody  pitch="+30%">could you try again? </prosody> </p> </speak>`,
   `<speak> <s> This is embarrassing for me </s> <p> I sometimes have trouble with my hearing </p> <p> even at such a young age </p> <s> Would you kindly try the command again? </s> </speak>`
 ];
 const spanishErrorResponse = [];
