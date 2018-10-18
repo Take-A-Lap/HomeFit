82906013468ecb23c1399c3b8aa7b8c5e8e889c3
Alex Sfamurri <alex.sfamurri.504@gmail.com>
2018-10-18 14:08:20 -0500

    test for id 1 exercise description was good

--- back-end/googleAssHelpers/helpers.js
+++ back-end/googleAssHelpers/helpers.js
@@ -193,11 +193,11 @@ app.intent('describe exercise', conv => {
     conv.ask(`Hola, mi llamo alexa`);
   } else {
 
-    return db.getExerciseDescription(1)
+    return db.getExerciseDescription(2)
       .then(({ description }) =>{
         console.log('describe was invoked');
         
-        conv.ask('<speak> <prosody rate="fast"> ' + description + " </prosody> </speak>");
+        conv.ask('<speak> <prosody rate="medium"> ' + description + " </prosody> </speak>");
       })
     // conv.ask("<speak> This is the description for" + current.name +" </speak>");
     // conv.ask("<speak>" + current.description + "</speak>");
