be5d6044c1f43a044d936d0e1afcebc7ad7859d3
Joshua Strzeszkowski <joshstrzeszkowski@gmail.com>
2018-10-18 10:42:42 -0500

    changed styling of app to periwinkle on black, fixed weather image bug

--- back-end/server/index.js
+++ back-end/server/index.js
@@ -137,7 +137,8 @@ app.post('/weather', (req, res) => {
     })
     .then(() => db.getWeatherImages(weatherInfo.text, weatherInfo.time_of_day))
     .then(result => {
-      weatherInfo.url = result
+      weatherInfo.url = result.url
+      console.log(weatherInfo.url)
     })
     .then(() => {
       res.send(weatherInfo)
@@ -356,7 +357,7 @@ alexaRouter.post('/fitnessTrainer', (req, res) => {
   }
 });
 
-const port = 81;
+const port = 3000;
 app.listen(port, () => {
   console.log(`HomeFit is listening on port ${port}!`);
   app.keepAliveTimeout = 0;
