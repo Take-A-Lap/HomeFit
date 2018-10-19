9fd241dc54c4618ae8fcc70bce26e8d8ce7c2160
Alex Sfamurri <alex.sfamurri.504@gmail.com>
2018-10-18 20:37:35 -0500

    test 47 went well

--- back-end/googleAssHelpers/helpers.js
+++ back-end/googleAssHelpers/helpers.js
@@ -192,7 +192,7 @@ app.intent('describe exercise', conv => {
     conv.ask(`Hola, mi llamo alexa`);
   } else {
 
-    return db.getExerciseDescription(47)
+    return db.getExerciseDescription(48)
       .then(({ description }) =>{
         
         conv.ask('<speak> <prosody pitch="+16%"> ' + description + " </prosody> </speak>");
