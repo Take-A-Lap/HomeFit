d34313e7a159565abc06f0a00094e3f9f2fe70ed
Alex Sfamurri <alex.sfamurri.504@gmail.com>
2018-10-19 13:49:44 -0500

    changed the third layot of the linkaccount response

--- back-end/googleAssHelpers/engResponse.js
+++ back-end/googleAssHelpers/engResponse.js
@@ -55,8 +55,28 @@ const linkAccountObjResponses = [
     </speak>`
   },
   {
-    before: '<speak> <s> <prosody pitch="-5%" rate="slow"> Well </prosody> </s> <p> <s>',
-    after: '</s> </p> <break time="5ms" /> <prosody rate="fast"><s> Looks like we are all set </s> <s> lets get started with those exercises </s> </prosody> </speak>'
+    before: `
+    <speak> 
+      <s> 
+        <prosody pitch="-5%" rate="slow">
+          Well 
+        </prosody> 
+      </s> 
+      <p> 
+        <s>`,
+    after: `
+        </s> 
+      </p> 
+      <break time="5ms" /> 
+      <prosody rate="fast">
+        <s> 
+          Looks like we are all set 
+        </s> 
+        <s> 
+          lets get started with those exercises 
+        </s> 
+      </prosody> 
+    </speak>`
   },
   {
     before: '<speak> <prosody rate="fast"> <s> <prosody pitch="+20%">>Glad to have you back ',
