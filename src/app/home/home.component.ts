22a421602639caa3a7d5aa4e37d993e161717aa1
Cornelius <brcornelius@gmail.com>
2018-10-23 19:45:15 -0500

    trying to log out on the live remote

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -225,7 +225,7 @@ export class HomeComponent implements OnInit {
         document.cookie = name +
           '=; expires=Thu, 01-Jan-70 00:00:01 GMT;';
        
-      if (!document.cookie){
+      if (document.cookie){
         reject('Could not delete cookie')
       } else {
         resolve('success')
@@ -234,10 +234,8 @@ export class HomeComponent implements OnInit {
   }
   logout(){
     const cookie = document.cookie
-    console.log(cookie)
     if(cookie){
       this.deleteCookie(cookie).then(() => this.splash())
-      console.log(document.cookie)
     } else {
       this.splash();
     }
