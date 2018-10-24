8d4e14f9d5717732f5577979a94b03ea709a422c
Cornelius <brcornelius@gmail.com>
2018-10-23 19:53:35 -0500

    added a couple console.logs

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -224,8 +224,8 @@ export class HomeComponent implements OnInit {
     return new Promise((resolve,reject)=>{
         document.cookie = name +
           '=; expires=Thu, 01-Jan-70 00:00:01 GMT;';
-       
-      if (document.cookie){
+       console.log(document.cookie)
+      if (document.cookie !== ''){
         reject('Could not delete cookie')
       } else {
         resolve('success')
