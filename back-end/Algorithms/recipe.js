032355b032f8c88c43800901e2e67c2eefd6c98e
Joshua Strzeszkowski <joshstrzeszkowski@gmail.com>
2018-10-25 19:32:36 -0500

    added console.log on line 339 index.js, removed console.log from recipe.js

--- back-end/Algorithms/recipe.js
+++ back-end/Algorithms/recipe.js
@@ -9,7 +9,6 @@ const axios = require('axios');
 
 const getMeal = function (meat, calorieMin, calorieMax, dietaryRestrictions) {
   const adjustment = dietaryRestrictions ? dietaryRestrictions.map(restriction => `&${restriction.type}=${restriction.name}`).reduce((acc, curr) => acc.concat(curr), '') : '';
-  console.log([meat, adjustment])
   return axios.get(`https://api.edamam.com/search?q=${meat}&app_id=${config.EDAMAM_API_ID}&app_key=${config.EDAMAM_API_KEY}&from=0&to=30&calories=${calorieMin}-${calorieMax}${adjustment}`)
     .then(recipes => recipes.data.hits)
 };
